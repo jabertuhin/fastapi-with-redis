d92f1ccee81b8bcc99c0085e4970b3406c327ffb
jabertuhin <jabertuhin@gmail.com>
2021-08-14 13:42:00 +0600

    Add APIKeyAuthenticator interface.

--- app/authentication/api_key_authenticator.py
+++ app/authentication/api_key_authenticator.py
@@ -2,13 +2,6 @@ from abc import ABC
 from typing import NoReturn
 from fastapi.security import APIKeyHeader
 
-API_KEY_NAME = "x-api-key"
-
-
-api_key_header = APIKeyHeader(name=API_KEY_NAME, 
-                            scheme_name="API Key",
-                            auto_error=True)
-
 
 class APIKeyAuthenticator(ABC):
     async def authenticate(api_key: str) -> NoReturn:
