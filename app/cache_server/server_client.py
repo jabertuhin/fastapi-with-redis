2b8c498a0ef6364d57589c6985c0e5c3e044f1db
jabertuhin <jabertuhin@gmail.com>
2021-08-14 13:48:04 +0600

    Update CacheServerClient interface.

--- app/cache_server/server_client.py
+++ app/cache_server/server_client.py
@@ -4,11 +4,11 @@ from typing import NoReturn
 
 class CacheServerClient(ABC):
     """An interface for cache server."""
-    async def set_key_value(key: str, value: int)-> NoReturn:
+    async def set_key_value(self, key: str, value: int)-> NoReturn:
         raise NotImplementedError()
 
-    async def get_value(key: str)-> int:
+    async def get_value_by_key(self, key: str)-> int:
         raise NotImplementedError()
 
-    async def increment_value(key: str, value: int)-> NoReturn:
+    async def increment_value_by_key(self, key: str)-> NoReturn:
         raise NotImplementedError()
