a8e8c387ea8519901bf020587e0a0128556dd184
jabertuhin <jabertuhin@gmail.com>
2021-08-20 23:06:15 +0600

    Add authenticator package.

--- app/authentication/api_key_authenticator.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC
-from typing import NoReturn
-from fastapi.security import APIKeyHeader
-
-
-class APIKeyAuthenticator(ABC):
-    async def authenticate(api_key: str) -> NoReturn:
-        raise NotImplementedError()
