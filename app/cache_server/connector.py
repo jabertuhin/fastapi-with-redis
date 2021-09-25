ac1fcff5a8a3941de213dc1c85e6a68851c9fc40
jabertuhin <jabertuhin@gmail.com>
2021-09-25 12:14:02 +0600

    Add abstractmethod in Connector.

--- app/cache_server/connector.py
+++ app/cache_server/connector.py
@@ -1,4 +1,11 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 
-class Connector(ABC):    
-    pass
+
+class Connector(ABC):
+    @abstractmethod
+    async def __aenter__(self):
+        raise NotImplementedError()
+
+    @abstractmethod
+    async def __aexit__(self, exc_type, exc_value, exc_tb) -> None:
+        raise NotImplementedError()
