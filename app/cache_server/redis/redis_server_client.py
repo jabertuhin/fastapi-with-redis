fad4649306bcbcdd68ae644d26dad97d33d39bf8
jabertuhin <jabertuhin@gmail.com>
2021-09-25 12:15:35 +0600

    Formatting and instance variable name change.

--- app/cache_server/redis/redis_server_client.py
+++ app/cache_server/redis/redis_server_client.py
@@ -1,4 +1,3 @@
-from typing import NoReturn
 import logging
 
 from fastapi import Depends
@@ -11,31 +10,34 @@ logger = logging.getLogger(__name__)
 
 
 class RedisServerClient(CacheServerClient):
-    def __init__(self, connector: Connector = Depends(RedisConnector)) -> NoReturn:
+    def __init__(self, connector: Connector = Depends(RedisConnector)) -> None:
         super().__init__()
-        self.connector = connector   
+        self._connector = connector
 
-    async def set_key_value_with_expiry_time(self, key: str, value: int = 1, expires_in_second: int = 60)-> NoReturn:
-        async with self.connector as redis:                        
+    async def set_key_value_with_expiry_time(self,
+                                             key: str,
+                                             value: int = 1,
+                                             expires_in_second: int = 60) -> None:
+        async with self._connector as redis:
             logger.debug("Setting key-value in redis.")
             await redis.set(key, value, expire=expires_in_second)
 
-    async def get_value_by_key(self, key: str)-> str:
+    async def get_value_by_key(self, key: str) -> bytes:
         try:
-            async with self.connector as redis:                
+            async with self._connector as redis:
                 logger.debug("Getting value from redis by key.")
-                value = await redis.get(key)                            
+                value = await redis.get(key)
             return value
         except Exception as excep:
             logger.exception(excep)
             raise
 
-    async def increment_value_by_key(self, key: str)-> NoReturn:
-        async with self.connector as redis:
-            logger.debug("Incrementing key's value in Redis.")            
+    async def increment_value_by_key(self, key: str) -> None:
+        async with self._connector as redis:
+            logger.debug("Incrementing key's value in Redis.")
             await redis.incr(key)
 
     async def key_exists(self, key: str) -> int:
-        async with self.connector as redis:
+        async with self._connector as redis:
             logger.debug("Checking if key exists in Redis.")
             return await redis.exists(key)
